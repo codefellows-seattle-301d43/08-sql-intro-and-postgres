d8d4a9bb2a80f3f5e0f264f27262dfc11967cf02
ellisken <ellisken@oregonstate.edu>
2018-09-13 09:47:54 -0700

    comment 4 in server.js done

--- richard-kendra/server.js
+++ richard-kendra/server.js
@@ -75,7 +75,7 @@ app.post('/articles', (request, response) => {
 
 app.put('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
-  // PUT YOUR RESPONSE HERE
+  // This also matches #3 in the diagram, since it queries the DB. It's used by Article.updateRecord to update a record in the DB. This matches the 'U' in CRUD, since it's updating the record.
 
   let SQL = '';
   let values = [];
