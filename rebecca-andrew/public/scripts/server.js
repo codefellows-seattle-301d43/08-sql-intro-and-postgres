cfb81c5c60250951256d2f26a9a02aafb6cb62f6
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-09-13 09:11:06 -0700

    Add necessary variables in server file

--- rebecca-andrew/public/scripts/server.js
+++ rebecca-andrew/public/scripts/server.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const app = express();
+
+// POST middleware from yesterday's starter code
+app.use(express.urlencoded({ extended: true }));
+
+// serves up public directory as root for static files
+app.use(express.static('public'));
+
+app.listen(PORT => {
+  console.log(`we are listening to things on port ${PORT}!!`);
+})
