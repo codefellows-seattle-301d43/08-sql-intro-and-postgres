934cedb169913ee5c05efec2191ce71542bc52c3
Jeff <jhwe92@gmail.com>
2018-09-13 10:41:33 -0700

    Finished all CRUD operations

--- jeff-kris/server.js
+++ jeff-kris/server.js
@@ -12,7 +12,7 @@ const app = express();
 // const conString = 'postgres://mnfmnfm:postgrespassword@localhost:5432/kilovolt';
 
 // Mac:
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://localhost:5432/kilovolt';
 
 const client = new pg.Client(conString);
 
@@ -76,9 +76,28 @@ app.post('/articles', (request, response) => {
 app.put('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // PUT YOUR RESPONSE HERE
+  console.log(request.params.id);
+  let SQL = `
+    UPDATE articles
+    SET
+      title = $1,
+      author = $2,
+      author_url = $3,
+      category = $4,
+      published_on = $5,
+      body = $6
 
-  let SQL = '';
-  let values = [];
+    WHERE article_id = $7;
+  `;
+  let values = [
+    request.body.title,
+    request.body.author,
+    request.body.author_url,
+    request.body.category,
+    request.body.published_on,
+    request.body.body,
+    request.params.id
+  ];
 
   client.query( SQL, values )
     .then(() => {
@@ -109,7 +128,7 @@ app.delete('/articles', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // PUT YOUR RESPONSE HERE
 
-  let SQL = '';
+  let SQL = 'DELETE FROM articles';
   client.query( SQL )
     .then(() => {
       response.send('Delete complete')
