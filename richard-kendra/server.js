6466968fb87f887c6658be86db88633795edeeb0
richjimdev <richjimdev@gmail.com>
2018-09-13 11:09:54 -0700

    fixed update article bug

--- richard-kendra/server.js
+++ richard-kendra/server.js
@@ -78,8 +78,25 @@ app.put('/articles/:id', (request, response) => {
   // COMMENT: What number(s) of the full-stack-diagram.png image correspond to the following line of code? Which method of article.js is interacting with this particular piece of `server.js`? What part of CRUD is being enacted/managed by this particular piece of code?
   // This also matches #3 & #5 in the diagram, since it queries the DB and responds to the client. It's used by Article.updateRecord to update a record in the DB. This matches the 'U' in CRUD, since it's updating the record.
 
-  let SQL = '';
-  let values = [];
+  let SQL = `UPDATE articles
+  SET
+    title=$1,
+    author=$2,
+    author_url=$3,
+    category=$4,
+    published_on=$5,
+    body=$6
+  WHERE article_id=$7;`;
+
+  let values = [
+    request.body.title,
+    request.body.author,
+    request.body.author_url,
+    request.body.category,
+    request.body.published_on,
+    request.body.body,
+    request.params.id
+  ];
 
   client.query( SQL, values )
     .then(() => {
